b6063649694c47f53ade691dd40376e27fe294be
jose <josesandeson54@gmail.com>
2021-08-13 13:17:39 -0300

    Configurado recarregamento automatico do projeto

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    console.log("hello world!");
+    res.send("Hello world");
+})
+
+app.post("/courses", (req,res) => {
+    const {name} = req.body;
+
+    return res.json({name});
+})
+
+app.listen(3333, () => console.log("Server is running!"));
+
