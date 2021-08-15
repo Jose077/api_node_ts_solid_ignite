73f3739653583b9cf7c9aee419e2b877bc412b64
jose <sandersonqueiroz@outlook.com>
2021-08-15 16:01:09 -0300

    Implementada criacao de specificacao

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,21 @@
 import express from 'express';
 
 import { categoriesRoutes } from './routes/categories.routes'
+import { specificationRoutes } from './routes/specification.routes';
 
 const app = express();
 
 app.use(express.json());
-app.use("/categories", categoriesRoutes);
 
-app.get("/", (req,res) => {
+app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationRoutes)
+
+app.get("/", (req, res) => {
     console.log("hello world!");
     res.send("Hello world");
 })
 
-app.post("/courses", (req,res) => {
-    const {name} = req.body;
 
-    return res.json({name});
-})
 
 app.listen(3333, () => console.log("Server is running!"));
 
