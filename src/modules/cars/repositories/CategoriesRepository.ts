049b77db2e64b8a73112ea0db03b05b8f6d52c14
jose <sandersonqueiroz@outlook.com>
2021-08-15 20:07:26 -0300

    Adicionada validacao para instanciar repositorio apensa uma vez

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -6,7 +6,17 @@ import { ICategoriesRespository, ICreateCategoryDTO } from "./ICategoriesReposit
 class CategoriesRepository implements ICategoriesRespository {
     private categories: Category[];
 
-    constructor() { this.categories = []; }
+    private static INSTANCE: CategoriesRepository
+
+    private constructor() { this.categories = []; }
+
+    public static getInstance(): CategoriesRepository{
+        if(!CategoriesRepository.INSTANCE){
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+
+        return CategoriesRepository.INSTANCE;
+    }
 
     create({name, description} : ICreateCategoryDTO): void{
         const category = new Category();
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,16 @@
 import { Router } from 'express'
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/usesCases/createCategory';
-import { CreateCategoryUseCase } from '../modules/cars/usesCases/createCategory/CreateCategoryUseCase';
+import { listCategoriesController } from '../modules/cars/usesCases/listCategories';
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (req,res) => {
     return createCategoryController.handle(req, res);
 })
 
 categoriesRoutes.get('/', (req,res) => {
-   const listCategories = categoriesRepository.list();
-
-   return res.status(200).send(listCategories);
+    return listCategoriesController.handle(req, res);
 })
 
 export { categoriesRoutes }
