f9a840147b8171f50d881b6729ae172edef0a421
jose <sandersonqueiroz@outlook.com>
2021-08-15 21:59:52 -0300

    Salvando dados da leitura do upload na base de dados

--- src/modules/cars/usesCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/usesCases/importCategory/ImportCategoryUseCase.ts
@@ -1,21 +1,55 @@
 import fs from "fs"
 import csvParse from "csv-parse"
+import { ICategoriesRespository } from "../../repositories/ICategoriesRepository";
+import { resolve } from "path/posix";
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 
 class ImportCategoryUseCase {
-    execute( file: any ): void {
-        const stream = fs.createReadStream(file.path);
+    constructor(private categoriesRepository: ICategoriesRespository){}
 
-        const parseFile = csvParse();
+    loadCategories(file: any): Promise<IImportCategory[]> {
+     
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
 
-        stream.pipe(parseFile);
-
-        parseFile.on("data", async (line) => {
-            console.log(line);
-            
+            const categories: IImportCategory[] = []
+    
+            const parseFile = csvParse();
+    
+            stream.pipe(parseFile);
+    
+            parseFile.on("data", async (line) => {
+                const [name, description] = line; 
+                categories.push(name, description)          
+            }).on("end", () => {
+                resolve(categories)
+            }).on("error", (err) => {
+                reject(err)
+            })
         })
-        
+
     }
 
+   async  execute( file: any ): Promise<void> {
+        const categories = await this.loadCategories(file);
+
+        categories.map(async (category) => {
+            const { name, description } = category;
+
+            const existCategory = this.categoriesRepository.findByName(name);
+
+            if(!existCategory){
+                this.categoriesRepository.create({
+                    name, 
+                    description
+                });
+            }
+
+        }) 
+    }
 }
 
 export { ImportCategoryUseCase}
