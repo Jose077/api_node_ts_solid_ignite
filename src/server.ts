5d93b027916dba04d145db56c60a5eb0fcea308e
jose <sandersonqueiroz@outlook.com>
2021-08-15 21:00:34 -0300

    Adicionado o multer e criada rota para leitura de arquivos

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,11 @@
 import express from 'express';
-
-import { categoriesRoutes } from './routes/categories.routes'
-import { specificationRoutes } from './routes/specification.routes';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationRoutes)
-
-app.get("/", (req, res) => {
-    console.log("hello world!");
-    res.send("Hello world");
-})
-
-
+app.use(router)
 
 app.listen(3333, () => console.log("Server is running!"));
 
