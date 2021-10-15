df984200db22a675571baa959728c4aa7728649c
jose <sandersonqueiroz@outlook.com>
2021-10-15 15:11:40 -0300

    Criado controller para devolucao de veiculo

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -2,15 +2,15 @@ import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
 import { Rental } from "../infra/typeorm/entities/Rental";
 
 
-
-
 interface IRentalsRepository {
 
     findOpenRentalByCar(car_id: string): Promise<Rental>;
 
-    findOpenRentalByUser(user_id): Promise<Rental>
+    findOpenRentalByUser(user_id): Promise<Rental>;
 
-    create(data: ICreateRentalDTO): Promise<Rental>
+    create(data: ICreateRentalDTO): Promise<Rental>;
+
+    findById(id: string): Promise<Rental>
 
 }
 
