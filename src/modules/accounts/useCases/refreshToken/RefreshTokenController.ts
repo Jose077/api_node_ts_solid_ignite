ee598b518e27a46cc1513eb6c73b0aff57d979a5
jose <sandersonqueiroz@outlook.com>
2021-10-18 10:32:44 -0300

    Criad usecase de refresh token

---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -0,0 +1,11 @@
+
+import { Request, Response} from "express"
+
+
+class RefreshTokenController {
+    async handle(req: Request, res: Response): Promise<Response> {
+        return await res.status(201)
+    }
+}
+
+export { RefreshTokenController }
